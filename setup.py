65d12342e8b3ee0eb8407ccabe286703cd7eb00f
Steve <stautonico@gmail.com>
2019-07-11 15:57:24 -0400

    Release 1.0.3
    Fixed distribution script

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='PyDocParser',
-    version='1.0',
-    packages=['pydocparser'],
+    version='1.0.3',
+    packages=find_packages(),
+    include_package_data=True,
     url='https://github.com/tman540/pydocparser',
     license='MIT',
     author='Steve Tautonico',
     author_email='stautonico@gmail.com',
     description='A python client for the DocParser API',
-    long_description=open("README.md").read(),
-	install_requires=["requests>=2.22.0"],
-	keywords=["docparser", "API",],
-	long_description_content_type="text/markdown"
-	
+    long_description=README,
+    install_requires=["requests>=2.22.0"],
+    keywords=["docparser", "API"],
+    long_description_content_type="text/markdown"
+
 )
